deec7dfac1e8bb0c23d29ed4395a5f4b4e54403b
Dev Dabke <dev@dabke.com>
2020-05-13 02:55:00 -0400

    Add SVD Graph model

--- kge/model/__init__.py
+++ kge/model/__init__.py
@@ -2,6 +2,7 @@ from kge.model.kge_model import KgeModel, KgeEmbedder
 
 # embedders
 from kge.model.embedder.lookup_embedder import LookupEmbedder
+from kge.model.embedder.graph_embedder import GraphEmbedder
 from kge.model.embedder.projection_embedder import ProjectionEmbedder
 from kge.model.embedder.tucker3_relation_embedder import Tucker3RelationEmbedder
 
@@ -12,6 +13,7 @@ from kge.model.distmult import DistMult
 from kge.model.relational_tucker3 import RelationalTucker3
 from kge.model.rescal import Rescal
 from kge.model.svd import Svd
+from kge.model.svd_graph import SvdGraph
 from kge.model.transe import TransE
 from kge.model.rotate import RotatE
 from kge.model.cp import CP
---
+++ kge/model/svd_graph.py
@@ -0,0 +1,92 @@
+import math
+import torch
+import time
+
+from kge import Config, Dataset
+from kge.model.kge_model import KgeEmbedder, KgeModel, RelationalScorer
+
+
+class SvdGraphScorer(RelationalScorer):
+    r"""Implementation of the SVD Graph KGE scorer."""
+
+    def __init__(self, config: Config, dataset: Dataset, configuration_key=None):
+        super().__init__(config, dataset, configuration_key)
+
+    def score_emb(
+        self,
+        s_emb: torch.Tensor,
+        p_emb: torch.Tensor,
+        o_emb: torch.Tensor,
+        combine: str,
+    ):
+        rank = self.get_option("rank")
+        batch_size = p_emb.size(0)
+        entity_size = s_emb.size(-1)
+
+        p_matrix_decomp = p_emb.view(-1, entity_size, 2 * rank)
+        u_matrices = p_matrix_decomp[:, :, 0:rank]
+        v_matrices = p_matrix_decomp[:, :, rank:(2 * rank)].permute(0, 2, 1)
+
+        if combine == "spo":
+            out = (
+                s_emb.unsqueeze(1)  # [batch x 1 x entity_size]
+                .bmm(u_matrices)  # apply mixing matrices
+                .bmm(v_matrices)
+                .view(batch_size, entity_size)  # drop dim 1
+                * o_emb  # apply object embeddings
+            ).sum(
+                dim=-1
+            )  # and sum to obtain predictions
+        elif combine == "sp_":
+            out = (
+                s_emb.unsqueeze(1)
+                .bmm(u_matrices)
+                .bmm(v_matrices)
+                .view(batch_size, entity_size)
+                .mm(o_emb.transpose(0, 1))
+            )
+        elif combine == "_po":
+            out = (
+                u_matrices.bmm(v_matrices).bmm(o_emb.unsqueeze(2))
+                .view(batch_size, entity_size)
+                .mm(s_emb.transpose(0, 1))
+            )
+        else:
+            super().score_emb(s_emb, p_emb, o_emb, combine)
+
+        return out.view(batch_size, -1)
+
+
+class SvdGraph(KgeModel):
+    r"""Implementation of the proposed SVD Graph KGE model."""
+
+    def __init__(self, config: Config, dataset: Dataset, configuration_key=None):
+        self._init_configuration(config, configuration_key)
+        svd_set_relation_embedder_dim(
+            config, dataset, self.configuration_key
+        )
+        super().__init__(
+            config, dataset, SvdGraphScorer, configuration_key=self.configuration_key
+        )
+
+
+def svd_set_relation_embedder_dim(config, dataset, conf_key):
+    """Set the relation embedder dimensionality for SVD in the config.
+
+    If <0, set it to 2 * rank * emb_dim + rank. Else leave
+    unchanged.
+
+    """
+    rel_emb_conf_key = conf_key + ".relation_embedder"
+    rank = config.get_default(conf_key + ".rank")
+    dim = config.get_default(rel_emb_conf_key + ".dim")
+    if dim < 0:  # autodetect relation embedding dimensionality
+        ent_emb_conf_key = rel_emb_conf_key.replace(
+            "relation_embedder", "entity_embedder"
+        )
+        if ent_emb_conf_key == rel_emb_conf_key:
+            raise ValueError(
+                "Cannot determine relation embedding size; please set manually."
+            )
+        dim = 2 * config.get_default(ent_emb_conf_key + ".dim") * rank
+        config.set(rel_emb_conf_key + ".dim", dim, log=True)
